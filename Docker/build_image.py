2018c8dbe702f47fe48e5f497f7d9dfaa281aaa6
Nobody <nobody@nowhere.to.go>
2015-11-09 20:35:06 -0500

    Working version

---
+++ Docker/build_image.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import subprocess
+import shutil
+
+def main():
+    """
+    Building Docker image for Ubuntu baseline
+    """
+
+    # step last - build docker image
+    rc = subprocess.call(["docker", "build", "-t", "ubuntu:baseline",  "."], stderr=subprocess.PIPE)
+    if rc != 0:
+        raise RuntimeError("Unable to build docker image")
+
+    return rc
+
+if __name__ == '__main__':
+    sys.exit(main())
